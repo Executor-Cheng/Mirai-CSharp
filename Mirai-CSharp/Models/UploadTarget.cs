3c42e4fdbbdbdc684610f4cda377f5a8992c19d8
Executor <847529602@qq.com>
2020-08-22 16:29:36 +0800

    添加收发语音支持 Breaking Changes: 对VoiceMessage类的属性命名有重大更改 由于mirai-api-http尚未支持最新的mirai-console, 暂时无法进行测试, 先作为draft以后再测

--- Mirai-CSharp/Models/UploadTarget.cs
+++ Mirai-CSharp/Models/UploadTarget.cs
@@ -1,12 +1,9 @@
-﻿using System;
-
-namespace Mirai_CSharp.Models
+﻿namespace Mirai_CSharp.Models
 {
     /// <summary>
-    /// 图片的目标类型
+    /// 上传对象的目标类型
     /// </summary>
-    [Obsolete("请使用 UploadTarget")]
-    public enum PictureTarget
+    public enum UploadTarget
     {
         /// <summary>
         /// 好友
---
+++ Mirai-CSharp/Session/MiraiHttpSession.SendVoice.cs
@@ -0,0 +1,72 @@
+﻿using Mirai_CSharp.Models;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+#pragma warning disable CS1573 // 参数在 XML 注释中没有匹配的 param 标记(但其他参数有) // 已经 inheritdocs, 警告无效
+namespace Mirai_CSharp
+{
+    public partial class MiraiHttpSession
+    {
+        /// <summary>
+        /// 内部使用
+        /// </summary>
+        /// <exception cref="NotSupportedException"/>
+        /// <param name="type">目标类型</param>
+        /// <param name="voiceStream">语音流</param>
+        /// <returns>一个 <see cref="VoiceMessage"/> 实例, 可用于以后的消息发送</returns>
+        private Task<VoiceMessage> InternalUploadVoiceAsync(InternalSessionInfo session, UploadTarget type, Stream voiceStream)
+        {
+            if (session.ApiVersion < new Version(1, 8, 0))
+            {
+                throw new NotSupportedException($"当前版本的mirai-api-http不支持上传语音。({session.ApiVersion}, 必须>=1.8.0)");
+            }
+            HttpContent sessionKeyContent = new StringContent(session.SessionKey);
+            sessionKeyContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+            {
+                Name = "sessionKey"
+            };
+            HttpContent typeContent = new StringContent(type.ToString().ToLower());
+            typeContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+            {
+                Name = "type"
+            };
+            HttpContent voiceContent = new StreamContent(voiceStream);
+            voiceContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+            {
+                Name = "voice",
+                FileName = $"{Guid.NewGuid():n}.amr"
+            };
+            HttpContent[] contents = new HttpContent[]
+            {
+                sessionKeyContent,
+                typeContent,
+                voiceContent
+            };
+            return InternalHttpPostNoSuccCodeAsync<VoiceMessage, VoiceMessage>(session.Client, $"{session.Options.BaseUrl}/uploadImage", contents, session.Token);
+        }
+        /// <summary>
+        /// 异步上传语音
+        /// </summary>
+        /// <exception cref="FileNotFoundException"/>
+        /// <param name="voicePath">语音路径</param>
+        /// <inheritdoc cref="InternalUploadVoiceAsync"/>
+        public Task<ImageMessage> UploadVoiceAsync(UploadTarget type, string voicePath)
+        {
+            InternalSessionInfo session = SafeGetSession();
+            return InternalUploadPictureAsync(session, type, new FileStream(voicePath, FileMode.Open, FileAccess.Read, FileShare.Read));
+        }
+        /// <summary>
+        /// 异步上传语音
+        /// </summary>
+        /// <param name="voice">语音流</param>
+        /// <inheritdoc cref="InternalUploadVoiceAsync"/>
+        public Task<ImageMessage> UploadVoiceAsync(UploadTarget type, Stream voice)
+        {
+            InternalSessionInfo session = SafeGetSession();
+            return InternalUploadPictureAsync(session, type, voice);
+        }
+    }
+}
